b71da999c44c32b19fe6977c3432c119f55cf7ac
Cavalcante111 <danielcavalcante@hotmail.com>
2025-11-21 20:54:03 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -12,6 +12,24 @@ selected_season = st.sidebar.selectbox('Season', ['2022/2021', '2021/2020', '202
 
 # Webscrapping football-data
 def load_data(league, season):
+    if selected_league == 'England' :
+        league = 'E0'
+    if selected_league == 'Germany' :
+        league = 'D1'
+    if selected_league == 'Italy' :
+        league = 'I1'
+    if selected_league == 'Spain' :
+        league = 'SP1'
+    if selected_league == 'France' :
+        league = 'F1'
+
+    if selecte_season == '2021/2022' :
+        season = '2122'
+    if selecte_season == '2020/2021' :
+        season = '2021'
+    if selecte_season == '2019/2020' :
+        season = '1920'
+        
     url = "https://www.football-data.co.uk/mmz4281/" + season + "/" + league + ".csv"
     data = pd.read_csv(url)
     return data
