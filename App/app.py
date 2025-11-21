0a89e87b05ba6e79f199d322f2b1b9e9a035140b
Cavalcante111 <danielcavalcante@hotmail.com>
2025-11-21 20:57:47 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -23,11 +23,11 @@ def load_data(league, season):
     if selected_league == 'France' :
         league = 'F1'
 
-    if selecte_season == '2021/2022' :
+    if selected_season == '2021/2022' :
         season = '2122'
-    if selecte_season == '2020/2021' :
+    if selected_season == '2020/2021' :
         season = '2021'
-    if selecte_season == '2019/2020' :
+    if selected_season == '2019/2020' :
         season = '1920'
         
     url = "https://www.football-data.co.uk/mmz4281/" + season + "/" + league + ".csv"
